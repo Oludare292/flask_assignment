f854adfe15abfac147f6543235a85a519f142c43
Oludare292 <Jolayemidare88@gmail.com>
2021-06-15 22:46:30 +0530

    fixed bugs

--- app.py
+++ app.py
@@ -1,40 +1,80 @@
-from flask import Flask, request, jsonify, flash, redirect, url_for,Response
+from flask import Flask, request, Response
 import requests
-import pytest
 import json
 import os
+import time
+
+# global variable to store download directory
+downloadDIR = "downloads"
 
 
-def demoapp():
-    app = Flask("demoapp")
+# appending epoch time to fileName to make it unqiue and won't replace the existing files
+def prepareFileName(fileName):
+    return str(time.time())+"_"+fileName
+
+# function creates the downloadDIR if not exists
+def handleDownloadDirectory():
+    if not os.path.exists(downloadDIR):
+        os.makedirs(downloadDIR)
+
+# main app hanlder
+def mainapp():
+    app = Flask("mainapp")
     app.secret_key = 'xlaksfjoi23iop236'
-    @app.route('/manage_file',methods=['GET','POST'])
+    # invoking directory handler on server start
+    handleDownloadDirectory()
+
+    @app.route('/manage_file', methods=['POST'])
     def handlerequest():
         arg = request.get_json()
+        response = {}
+        FileURL = "https://www.learningcontainer.com/wp-content/uploads/2020/04/sample-text-file.txt"
+        FileName = "downloads"
+        # error handler if argument not included in body
+        if "action" not in arg:
+            response["error"] = "action is required, expected values: read, download"
+            return Response(json.dumps(response), status=400, mimetype='application/json')
+        # optional feature to fetch data of dynamic file url
+        if "url" in arg:
+            FileURL = arg["url"]
+        if "file_name" in arg:
+            FileName = arg["file_name"]
+        # condition to handle file download
         if arg["action"] == "download":
-            print("downloading")
-            resp = requests.get("https://www.learningcontainer.com/wp-content/uploads/2020/04/sample-text-file.txt")
-            text = resp.text
-            print(text)
-            with open(os.getcwd()+"\\sample\\sample-text-file.txt",'w') as f:
-                f.write(text)
-                f.close()
-                return "200"
-            return 404
+            try:
+                resp = requests.get(FileURL)
+                text = resp.text
+                fileName = prepareFileName(FileName)
+                filePath = "\\downloads\\"+fileName+".txt"
+                with open(os.getcwd()+filePath, 'w') as f:
+                    f.write(text)
+                    f.close()
+                    response["status"] = "success"
+                    response["filePath"] = filePath.replace("\\", "/")
+                    return Response(json.dumps(response), status=200, mimetype='application/json')
+
+            except Exception as e:
+                response["error"] = str(e)
+                return Response(json.dumps(response), status=500, mimetype='application/json')
+         # condition to handle file download
         elif arg["action"] == "read":
-            print("reading")
-            resp = requests.get("https://www.learningcontainer.com/wp-content/uploads/2020/04/sample-text-file.txt")
-            text = resp.text
-            print(text)
-            return Response(text, mimetype='text/plain',)
-            #mimetype = 'text/plain'
-            #return (text, resp.status_code)
-        else : return "Invalid action"
-        return "200"
+            try:
+                resp = requests.get(FileURL)
+                text = resp.text
+                response["status"] = "success"
+                response["text"] = text
+                return Response(json.dumps(response), status=200, mimetype='application/json')
+            except Exception as e:
+                response["error"] = str(e)
+                return Response(json.dumps(response), status=500, mimetype='application/json')
+        else:
+            # condition handles if action key value is invalid
+             response["error"] = "Invalid action, expected values: read, download"
+             return Response(json.dumps(response), status=400, mimetype='application/json')
     return app
+
 
 if __name__ == '__main__':
     print("Running Server")
-    app = demoapp()
+    app = mainapp()
     app.run()
-
